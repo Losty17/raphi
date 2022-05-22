a4875b6f6822cfd17587fba9a449e8babd173f37
Vinícius Kappke <viniciuskappke@gmail.com>
2022-05-21 21:52:23 -0300

    added presence

--- raphi/__main__.py
+++ raphi/__main__.py
@@ -1,5 +1,6 @@
 from os import getenv
 
+import discord
 from discord import Intents
 from dotenv import load_dotenv
 
@@ -14,6 +15,13 @@ bot = Raphi("", intents=intents, application_id=getenv("APP_ID"))
 
 @bot.event
 async def on_ready():
+    await bot.change_presence(
+        status=discord.Status.idle,
+        activity=discord.Activity(
+            name="cantigas de ninar",
+            type=discord.ActivityType.listening
+        )
+    )
     print(f'Logged in as {bot.user} (ID: {bot.user.id})')
 
 bot.run(getenv("BOT_TOKEN"))
