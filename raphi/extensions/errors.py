a715925d076dd00be8da7481891c5cf1d22ee5d9
Vinícius Kappke <viniciuskappke@gmail.com>
2022-05-22 00:46:03 -0300

    added error handling

---
+++ raphi/extensions/errors.py
@@ -0,0 +1,70 @@
+from socket import timeout
+from traceback import format_exception
+
+import discord
+from discord import Interaction, app_commands
+from discord.app_commands import AppCommandError
+from discord.ext import commands
+from raphi.raphi import Raphi
+
+
+class Errors(commands.Cog):
+    def __init__(self, bot: Raphi) -> None:
+        self.bot = bot
+
+        self.bot.tree.on_error = self.on_app_command_error
+
+    async def on_app_command_error(
+        self,
+        interaction: Interaction,
+        error: AppCommandError
+    ):
+        await self.handle_error(interaction, error)
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx: commands.Context, error: Exception):
+        await self.handle_error(ctx, error)
+
+    async def handle_error(self, ctx: commands.Context | Interaction, error: Exception | AppCommandError):
+        """Dynamic error handler for `app_commands` and `ext.commands`
+
+        Args:
+            ctx (`commands.Context` | `discord.Interaction`): error source
+            error (`Exception` | `AppCommandError`): error object
+        """
+        handle_origin = type(ctx)
+        user = ctx.author if handle_origin == commands.Context else ctx.user
+
+        # Select the right display message, or skip displaying one
+        match type(error):
+            case commands.CommandNotFound:
+                return
+
+            case app_commands.errors.CommandNotFound | discord.errors.NotFound:
+                emsg = "Não consegui interpretar sua solicitação"
+
+            case _:
+                emsg = f"Erro não tratado: `{type(error)}`"
+
+        # Print the traceback if the user is the owner
+        traceback = None
+        if error.__traceback__ and user.id == self.bot.owner_id:
+            traceback = (
+                f"\n\nTraceback:\n" +
+                f"```\n" +
+                f"{''.join(format_exception(error, value=error, tb=error.__traceback__))}"
+                f"```"
+            )
+
+        msg = f"Ops, algo deu errado! {emsg} {traceback if traceback else ''}"
+
+        # Send the message the right way
+        if (handle_origin == commands.Context):
+            # Simulate an ephemeral message
+            await ctx.send(msg, delete_after=15)
+        else:
+            await ctx.response.send_message(msg, ephemeral=True)
+
+
+async def setup(bot: commands.Bot) -> None:
+    await bot.add_cog(Errors(bot))
