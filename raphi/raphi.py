7b0af0fafc766376e9248bd3f20c3639ef133ef7
Vinícius Kappke <viniciuskappke@gmail.com>
2022-05-22 21:08:28 -0300

    syncing config

--- raphi/raphi.py
+++ raphi/raphi.py
@@ -4,6 +4,7 @@ from traceback import print_exc
 from typing import List
 
 from discord import Intents, Object
+import discord
 from discord.ext import commands
 
 
@@ -30,14 +31,11 @@ class Raphi(commands.Bot):
 
     async def setup_hook(self) -> None:
         await self.load_modules(self.ext)
+        await self.sync()
+        # await self._clear_global_commands()
+        # await self._global_sync()
 
-        for g in self.glds:
-            # self.tree.clear_commands(guild=None)
-            self.tree.copy_global_to(guild=g)
-            await self.tree.sync(guild=g)
-            # await self.tree.sync()
-
-    async def load_modules(self, extensions: List[str]):
+    async def load_modules(self, extensions: List[str]) -> None:
         """Loads the specified extensions
 
         Args:
@@ -68,3 +66,35 @@ class Raphi(commands.Bot):
         )
 
         return [f'.{directory}.{e[:-3]}' for e in file_list if e.endswith('.py')]
+
+    async def sync(self) -> None:
+        """ Sync the commands to the specified guilds """
+        print('\nSyncing commands...', end=' ')
+        for g in self.glds:
+            self.tree.copy_global_to(guild=g)
+            await self.tree.sync(guild=g)
+        print('Ok!')
+
+    async def _global_sync(self) -> None:
+        """ Sync commands globally """
+        print('\nSyncing commands globally...')
+
+        for g in self.glds:
+            print('\nClearing guild commands', end=' ')
+            print(f'from: {g.id}', end=' ')
+            self.tree.clear_commands(guild=g)
+            print('Ok!', end=' Syncing... ')
+            await self.tree.sync(guild=g)
+            print('Ok!', end=' ')
+
+        print('\nSyncing...')
+        await self.tree.sync()
+        print('Done!')
+
+    async def _clear_global_commands(self) -> None:
+        print('\nClearing global commands...', end=' ')
+        self.tree.clear_commands(guild=None)
+        print('Ok!')
+        print('Syncing...', end=' ')
+        await self.tree.sync(guild=None)
+        print('Ok!')
